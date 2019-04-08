96d4f7bb5a875f2086e1096d762ea6ec4703d077
Wagner Dutra <wagner.dutra@soaexpert.com.br>
2019-04-08 10:22:29 -0300

    Storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,6 +3,18 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { linkTo } from '@storybook/addon-links';
 
-import { Welcome } from '@storybook/react/demo';
+import { Welcome, Button } from '@storybook/react/demo';
+
+import { action } from '@storybook/addon-actions';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
